89d985d1f29533e4664933070009529080d49bfd
gonatae2-hash <gonatae2@gmail.com>
2026-01-28 15:34:59 +0900

    과제제출

---
+++ 2-4test.c
@@ -0,0 +1,53 @@
+/* "#### 문제
+숫자 이동프로그램
+1, 2, 3으로 초기화된 세 변수의 값을 하나씩 왼쪽으로 이동하여 출력합니다.
+가장 왼쪽의 값은 가장 오른쪽으로 이동합니다.
+키보드로 엔터를 입력할 때 마다 값을 이동하며, 엔터 이외의 키를 입력하면 종료합니다.
+세변수의 값을 동시에 바꾸는 함수를 만들어 사용하여, 함수 원형은 다음과 같이 작성합니다.
+
+void rotate(int* pa, int* pb, int* pc);
+
+실행결과
+1 : 2 : 3 (Enter) // 최초 세 변수의 값 1, 2, 3 출력 
+2 : 3 : 1 (Enter) // 각 값을 왼쪽으로 이동, 가장 왼쪽의 값은 가장 오른쪽으로 이동 
+3 : 1 : 2 (Enter)
+1 : 2 : 3 (Enter)
+2 : 3 : 1x(Enter) // 다른 문자와 함께 엔터 입력하면 반복 종료 
+"
+*/
+
+#include <stdio.h>
+
+void rotate(int* pa, int* pb, int* pc);
+
+int main(void)
+{
+	int pa, pb, pc;
+	int  a = 1, b = 2, c = 3;
+	char d;
+	printf("%d : %d : %d", a, b, c);
+	
+	while (1)
+	{
+		scanf("%c", &d);
+		if (d == '\n')
+		{
+			rotate(&a, &b, &c);
+			printf("%d : %d : %d", a, b, c);
+		}
+		else
+		{
+			return 0;
+		}
+	}
+}
+
+void rotate(int* pa, int* pb, int* pc)
+{
+	int n;
+	n = *pa;
+	*pa = *pb;
+	*pb = *pc;
+	*pc = n;
+	
+}
