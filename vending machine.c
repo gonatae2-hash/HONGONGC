a8fa00f316ca430048f189627a102aa42e290abe
gonatae2-hash <gonatae2@gmail.com>
2026-01-30 14:10:21 +0900

    과제제출

--- vending machine.c
+++ vending machine.c
@@ -14,66 +14,86 @@
    불충분 시	-> 금액 추가 문구 -> 상품 출력 
    */
 
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
 
 int num();
 
+
 int main(void)
 {
 	int choice;
-	int i = 0;
-	int a = 1;
-
-   
+	
+	
+    int c = 0;
+    int pay = 0;
+    int d = 0;
 
 	char* names[] = { "포카리스웨트", "콜라", "사이다", "커피", "빼빼로" };
 	int prices[] = { 1000, 1200, 1000, 800, 1500 };
+    
 	printf("================자판기================\n");
-	for (i = 0; i < 5; i++)									// 메뉴 (종류, 금액) 출력
+    int i = 0;
+    int a = 1;
+    for (i = 0; i < 5; i++)									// 메뉴 (종류, 금액) 출력
 	{
 		printf("%d. %s (%d원)\n", a, names[i], prices[i]);
 		a = a + 1;
 	}
 	printf("======================================\n");
+    
 
     int pick;
     pick = num();
-    if ((pick > 0) && (pick < 6))
+  
+    // pick 1~5번 사이 입력시 메뉴 정보 반환
+    if ((pick >= 1) && (pick <= 5))
     {
-        printf("\n%d. %s (%d원)\n\n", pick+1, names[pick], prices[pick]);
+        printf("%d. %s (%d원)\n", pick, names[pick-1], prices[pick-1]);  
     }
-    printf("금액을 넣어주십시오 :");
-
+   
+    printf("금액을 넣어주십시오 :\n");
+    
+    scanf("%d", &pay);
+    
+    d = pay - prices[pick - 1];                             // 거스름 돈 계산
+    
+    if (d != 0)
+    {
+        printf("잔돈 %d원이 반환됩니다.\n",d);
+    }
 }
+
 int num()
 {
-    int i=0;
-   
+    int i;
     printf("메뉴를 선택하세요 :");
     scanf("%d", &i);
-    if  (i == 1)
-    {
-        return 0;
-    }
-    else if (i == 2)
+
+    if (i==1)
     {
         return 1;
     }
-    else if (i == 3)
+    if (i == 2)
     {
         return 2;
     }
-    else if (i == 4)
+    if (i == 3)
     {
         return 3;
     }
-    else if (i == 5)
+    if (i == 4)
     {
         return 4;
     }
+    if (i == 5)
+    {
+        return 5;
+    }
     else
     {
-        return i;
+        printf("잘못된 번호입니다.\n다시 입력해 주세요.\n\n");
+        return num();
     }
 }
